b1e540e264ad19ae4a169137665cdf8f21b7c1fb
VikramVikrant <mensharetoo@gmail.com>
2020-07-25 11:15:51 +0530

    second commit

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+posts=[
+    {'author':'Vikram Vikrant','title':'Blog-post 1','content':'First Post Content','date':'july 24,2020'},
+    {'author':'Rahul Sharma','title':'Blog-post 2','content':'Second Post Content','date':'july 25,2020'},
+]
 # Create your views here.
 def home(request):
-    return render(request,'blog/home.html')
+    context={
+        'posts':posts
+    }
+
+    return render(request,'blog/home.html',context)
 
 def about(request):
-        return render(request,'blog/about.html')
+        return render(request,'blog/about.html',{'title':'About Page'})
